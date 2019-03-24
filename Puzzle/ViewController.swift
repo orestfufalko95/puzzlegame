5c88fcd7f393820f46c7bf29054b867230770e68
orestfufalko <orestfufalko.eleks@gmail.com>
2019-03-24 19:30:39 +0200

    Initial Commit

---
+++ Puzzle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Puzzle
+//
+//  Created by Orest Fufalko on 3/24/19.
+//  Copyright © 2019 Orest Fufalko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
